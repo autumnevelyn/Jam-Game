8cd3206ad3949350aebbd3a43f02165cdb6c2cf0
Arena78 <103265035+Arena78@users.noreply.github.com>
2026-07-22 19:49:20 +0200

    added attacks

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,5 +1,8 @@
 extends CharacterBody2D
 
+@onready var timer: Timer = $Timer
+@onready var attack_hitbox: Area2D = $attack_hitbox
+@onready var texture_progress_bar: TextureProgressBar = $TextureProgressBar
 
 const movement_speed = 150;
 
@@ -17,9 +20,27 @@ func _physics_process(delta: float) -> void:
 		velocity.y = directionY * movement_speed
 	else:
 		velocity.y = move_toward(velocity.y, 0, movement_speed)
+	
+	
+	if(directionX != 0):
+		attack_hitbox.position.x = directionX * 16;
+	elif(directionY != 0): attack_hitbox.position.x = 0;
+	if(directionY != 0):
+		attack_hitbox.position.y = directionY * 16; 
+	elif(directionX != 0): attack_hitbox.position.y = 0;
+	if Input.is_action_pressed("ui_accept"):
+		timer.start();
+		
+		
+	texture_progress_bar.value = (timer.time_left / timer.wait_time) * 100;
 
 	move_and_slide()
 	
 	
 func attack():
-	pass
+	attack_hitbox.active = true;
+
+
+func _on_timer_timeout() -> void:
+	attack();
+	
