6be10fa1d2a994690d9b172a64154f02fff0f8a9
Arena78 <103265035+Arena78@users.noreply.github.com>
2026-07-24 23:25:03 +0200

    game over works now

--- scripts/ui/game_over.gd
+++ scripts/ui/game_over.gd
@@ -2,6 +2,8 @@ extends Control
 
 const MENU = preload("res://scenes/levels/menu.tscn")
 
+@onready var color_rect: ColorRect = $ColorRect
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	
@@ -16,22 +18,26 @@ func _process(delta: float) -> void:
 func _on_player_died(data: Dictionary):
 	var tween = create_tween();
 	tween.tween_property(self, "modulate:a", 1.0, 1.0);
+	
 
 func _on_button_pressed() -> void:
 	GameManager.start_run();
+	var tween = create_tween();
+	tween.tween_property(self, "modulate:a", 0.0, 1.0);
 
 
 func _on_button_3_pressed() -> void:
 	var tween = create_tween();
-	tween.tween_property(self, "color:a", 1.0, 1.0);
+	tween.tween_property(color_rect, "color:a", 1.0, 1.0);
 	
 	await tween.finished;
 	
 	GameManager.current_level.queue_free();
 	GameManager.current_level = null;
 	
-	var menu = MENU.instantiate();
-	get_tree().root.get_node("GameMain/CanvasLayer").add_child(menu);
+	get_tree().root.get_node("GameMain/CanvasLayer/Menu").visible = true;
+	#var menu = MENU.instantiate();
+	#get_tree().root.get_node("GameMain/CanvasLayer").add_child(menu);
 	
-	
+	tween = create_tween();
 	tween.tween_property(self, "modulate:a", 0.0, 1.0);
--- scripts/ui/menu.gd
+++ scripts/ui/menu.gd
@@ -1,18 +1,8 @@
 extends Control
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
-
 func _on_button_pressed() -> void:
 	GameManager.start_run();
-	queue_free();
+	visible = false;
 
 
 func _on_button_3_pressed() -> void:
